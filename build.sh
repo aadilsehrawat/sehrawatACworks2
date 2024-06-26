2de38dc35f042750394095c7729da52e61a16138
unknown <aadilsehrawat@gmail.com>
2024-06-26 23:47:24 +0530

    Vercel Deployment Method 2 - Build Static v7

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
 echo "START BUILD"
+
+# Ensure the Python environment is set up
+python3.9 -m venv vercel-env
+source vercel-env/bin/activate
+
+# Install dependencies
 python3.9 -m pip install -r requirements.txt
+
+# Apply migrations
 python3.9 manage.py migrate
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput --clear
+
 echo "END BUILD"
